2cb184c36172cbeac1807d2cb4ab4c954f68316f
viccc287 <75047819+viccc287@users.noreply.github.com>
2021-10-27 20:09:22 -0500

    Parte 1 front
    alpha version con SCSS sin cambiar a Bootstrap

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+/*window.addEventListener("DOMContentLoaded", function(){
+
+    document.getElementById("toggle-nav").onclick =
+
+    function(){
+        const elems = document.getElementsByClassName("nav-element");
+        for (let elem of elems){
+            console.log(elem)
+            elem.setAttribute("style", "display:none;");
+        }
+        document.getElementById("nav").setAttribute("style","height:10vh")
+    };
+})*/
